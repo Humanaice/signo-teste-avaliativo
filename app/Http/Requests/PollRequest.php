73cd927ae83b6e0d33179c6aa6851d1861b93bc2
Humanaice <matheusnsantana@gmail.com>
2025-05-02 01:22:19 -0300

    add(back) : Requests

---
+++ app/Http/Requests/PollRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PollRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after:start_date',
+        ];
+    }
+}
